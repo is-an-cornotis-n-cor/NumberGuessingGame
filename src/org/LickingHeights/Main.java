d0ebdcd30113b3e4ee3a9c2e5b35992c4f75d903
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-10-28 09:23:02 -0400

    logic loop

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,8 +1,33 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int numberInMyHead;
+        int numberGuessed;
+        numberInMyHead = (int) (Math.random() * 100) + 1;
+        System.out.println("Guess a number 1-100");
+
+        do {
+
+            numberGuessed = keyboard.nextInt();
+
+          guessingGame(numberInMyHead, numberGuessed);
+
+        } while (numberGuessed != numberInMyHead);
+
+
+
     }
-}
+
+
+   
+
+
+
+
+
